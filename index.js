9356c0ed8f913c10766163e61845c82afe9fa17e
Yana Pertels <ypertels@gmail.com>
2018-08-05 10:31:02 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const welcome = require('./welcome');
+
+const app = express();
+
+/*
+ * parse application/x-www-form-urlencoded && application/json
+ */
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('<h2>You have reached Dr Eyemole.</h2> <p>Unfortunately, they are ' +
+  'currently on sabbatical so they will not be able to answer your requests right now.' +
+  ' Please <a src=https://www.eyemole.io> leave a message </a>. </p>');
+});
+
+/*
+ * Endpoint to receive events from Slack's Events API.
+ * Handles:
+ *   - url_verification: Returns challenge token sent when present.
+ *   - event_callback: Confirm verification token & handle `team_join` event.
+ */
+app.post('/events', (req, res) => {
+  switch (req.body.type) {
+    case 'url_verification': {
+      // verify Events API endpoint by returning challenge if present
+      res.send({ challenge: req.body.challenge });
+      break;
+    }
+    case 'event_callback': {
+      if (req.body.token === process.env.SLACK_VERIFICATION_TOKEN) {
+        const event = req.body.event;
+
+        // `team_join` is fired whenever a new user (incl. a bot) joins the team
+        // check if `event.is_restricted == true` to limit to guest accounts
+        if (event.type === 'team_join' && !event.is_bot) {
+          const { team_id, id } = event.user;
+          welcome.initialMessage(team_id, id);
+        }
+        res.sendStatus(200);
+      } else { res.sendStatus(500); }
+      break;
+    }
+    default: { res.sendStatus(500); }
+  }
+});
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`App listening on port ${process.env.PORT}!`);
+});
---
+++ welcome.js
@@ -0,0 +1,29 @@
+const qs = require('querystring');
+const axios = require('axios');
+const JsonDB = require('node-json-db');
+
+const db = new JsonDB('users', true, false);
+
+const postResult = result => console.log(result.data);
+
+// default message - edit to include actual ToS
+const message = {
+  token: process.env.SLACK_TOKEN,
+  as_user: true,
+  link_names: true,
+  text: 'Hello there, young one, and welcome to the ~cult~ cooperative!' +
+  'You are about to embark on a long journey, so take this handbook with you to find out all that you need to know about this bizzarre land you just entered:' +
+  'https://docs.google.com/document/d/1mGE-XIueb9B7I5oI_Ls2A-_f77MxrowYX4SNw2KViSM/edit?usp=sharing \n \n ' +
+  'If you have any questions or if you find any misinformation in the document or if you just want to say hi to the Old Ones, send them a message at #board or #exec and they will say hi back :eyemole:',
+};
+
+const initialMessage = (teamId, userId) => {
+
+    // send the default message as a DM to the user
+    message.channel = userId;
+    const params = qs.stringify(message);
+    const sendMessage = axios.post('https://slack.com/api/chat.postMessage', params);
+    sendMessage.then(postResult);
+};
+
+module.exports = { initialMessage };

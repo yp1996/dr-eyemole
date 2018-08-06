8f5691be1ce834628c03f0e750e861cd93f3e078
Yana Pertels <ypertels@gmail.com>
2018-08-06 10:47:11 -0700

    implementing delayed responses

--- slashCommand.js
+++ slashCommand.js
@@ -9,7 +9,7 @@ const slashCommand = (body) => new Promise((resolve, reject) => {
 	python.run("generate.py", function (err, results) { 
 		console.log(results);
 		resolve(results.toString());
-	}
+	});
 	//console.log(pythonProcess);
 	/**
 	pythonProcess.stdout.on("data", data =>{

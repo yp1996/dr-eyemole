667b1e4cb851ff3908d705c2abb25b0016a78da2
Yana Pertels <ypertels@gmail.com>
2018-08-06 10:46:29 -0700

    implementing delayed responses

--- slashCommand.js
+++ slashCommand.js
@@ -1,15 +1,22 @@
 
-const spawn = require("child_process").spawn;
-const pythonProcess = spawn('python', ['./generate.py'])
+var python = require('python-shell');
+//const spawn = require("child_process").spawn;
+//const pythonProcess = spawn('python', ['./generate.py'])
 
 const slashCommand = (body) => new Promise((resolve, reject) => {
 
 	console.log("launching the process...");
-	
+	python.run("generate.py", function (err, results) { 
+		console.log(results);
+		resolve(results.toString());
+	}
+	//console.log(pythonProcess);
+	/**
 	pythonProcess.stdout.on("data", data =>{
         resolve(data.toString()); 
     })
     pythonProcess.stderr.on("data", reject)
+    **/
 });
 
 module.exports = slashCommand;

de313d29b161d75f6c076a2f21d23de8f12fe590
Yana Pertels <ypertels@gmail.com>
2018-08-06 11:21:14 -0700

    implementing delayed responses

--- generate.py
+++ generate.py
@@ -1,9 +1,8 @@
-print("Eyemole: ")
+import tensorflow as tf
 from textgenrnn import textgenrnn
 import random
 import sys
 
-print("Eyemole: ")
 textgen = textgenrnn()
 textgen.load("weights.hdf5")
 print(textgen.generate(1, temperature=0.6, prefix="Eyemole: ", return_as_list = True)[0])

7f5991ec7c21d140252c4f1f7ffaf30206ca1984
Yana Pertels <ypertels@gmail.com>
2018-08-06 12:18:39 -0700

    implementing delayed responses

--- generate.py
+++ generate.py
@@ -5,11 +5,7 @@ from textgenrnn import textgenrnn
 import random
 
 
-print("Eyemole: ")
+textgen = textgenrnn()
+textgen.load("weights.hdf5")
+print(textgen.generate(1, temperature=0.6, prefix="Eyemole: ", return_as_list = True)[0])
 sys.stdout.flush()
-
-
-#textgen = textgenrnn()
-#textgen.load("weights.hdf5")
-#print(textgen.generate(1, temperature=0.6, prefix="Eyemole: ", return_as_list = True)[0])
-#sys.stdout.flush()

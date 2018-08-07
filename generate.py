1cfafd754b1534c51f525d212cef9d8764bf0213
Yana Pertels <ypertels@gmail.com>
2018-08-06 18:56:31 -0700

    slash commands

--- generate.py
+++ generate.py
@@ -3,7 +3,7 @@ import sys
 from textgenrnn import textgenrnn
 import random
 
-
+print("Eyemole: ")
 textgen = textgenrnn()
 textgen.load("weights.hdf5")
 print(textgen.generate(1, temperature=0.6, prefix="Eyemole: ", return_as_list = True)[0])
